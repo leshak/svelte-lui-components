e53cf6b0e15b77e2a0ff553f2cd790cca05e1df6
Alexey K. <leshak@leshak.ru>
2024-10-13 19:26:48 +0000

    fix

--- cmd/get.sh
+++ cmd/get.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
 
 echo "Downloading LUI components..."
+
+# Create a temporary directory and store its name in a variable.
+TEMPD=$(mktemp -d)
+ZIPFILE="$TEMPD/lui.zip"
+
+curl -o $ZIPFILE -s -L https://github.com/leshak/svelte-lui-components/archive/refs/heads/master.zip
+
+echo "Extracting..."
+unzip -qq $ZIPFILE "svelte-lui-components-master/lui/*" -d $TEMPD
+
+rm -rf src/lib/components/lui
+mkdir -p src/lib/components/lui
+mv $TEMPD/svelte-lui-components-master/lui/* src/lib/components/lui
+
+echo "Cleaning up..."
+rm -rf $TEMPD
+
+echo "Done!"
